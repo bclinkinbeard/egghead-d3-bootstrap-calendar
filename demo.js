3982988690993bb8da682e9883e3483a0058a4dd
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-12-23 11:18:07 -0500

    Color scale by data

--- demo.js
+++ demo.js
@@ -1,10 +1,18 @@
+var dataset = _.range(31).map(function () {
+  if (Math.round(Math.random())) {
+    return Math.random();
+  } else {
+    return -(Math.random());
+  }
+});
 
 var colorScale = d3.scale.linear()
-  .domain([0, 30])
-  .range(['white', 'green']);
+  .domain([-1, 0, 1])
+  .range(['red', 'white', 'green']);
 
 var dayCells = d3
   .selectAll('.cal-day-inmonth')
+  .data(dataset)
   .style('background-color', function (d, i) {
-    return colorScale(i);
+    return colorScale(d);
   });
