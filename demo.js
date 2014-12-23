3fed90db611bbb9c9688fbb2f6d2d144d0af5325
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-12-23 11:15:40 -0500

    Color scale by index

--- demo.js
+++ demo.js
@@ -1,4 +1,10 @@
 
+var colorScale = d3.scale.linear()
+  .domain([0, 30])
+  .range(['white', 'green']);
+
 var dayCells = d3
   .selectAll('.cal-day-inmonth')
-  .style('background-color', 'orange');
+  .style('background-color', function (d, i) {
+    return colorScale(i);
+  });
